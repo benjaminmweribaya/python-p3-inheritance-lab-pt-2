67528182aa47f9f5ab63451551b74f8e1a1ff5c3
benjaminmweribaya <b3njaminbaya@gmail.com>
2024-12-03 22:35:54 +0300

    feat: Reduce repeated code and enhance objects using inheritance. Define two classes, one of which inherits from another. Define methods in the child class that use the super keyword to inherit from and augment methods in the parent class. Accomplish complex programming tasks using knowledge from previous modules.

--- lib/student.py
+++ lib/student.py
@@ -1,5 +1,39 @@
 class Student:
-    pass
+    # Method to print a greeting message
+    def hello(self):
+        print("Hey there! I'm so excited to learn stuff.")
+    
+    # Method to raise hand
+    def raise_hand(self):
+        print("Pick me!")
 
 class ChattyStudent(Student):
-    pass
+    # Overriding the hello method
+    def hello(self):
+        # Call the hello method from the parent class
+        super().hello()
+        # Augment with chatty behavior
+        print("How are you doing today? I'm okay, but I'm kind of tired. "
+              "Did you watch The Walking Dead last night? You didn't?! "
+              "Oh man, it was so crazy! What, you don't want any spoilers? "
+              "Okay well let me just tell you who died...")
+    
+    # Overriding the raise_hand method
+    def raise_hand(self):
+        # Call the raise_hand method from the parent class 10 times
+        for _ in range(10):
+            super().raise_hand()
+
+# Testing the functionality
+if __name__ == "__main__":
+    # Create an instance of Student
+    student = Student()
+    print("Student says:")
+    student.hello()
+    student.raise_hand()
+    
+    print("\nChattyStudent says:")
+    # Create an instance of ChattyStudent
+    chatty_student = ChattyStudent()
+    chatty_student.hello()
+    chatty_student.raise_hand()            
